d69ab1a78c4d37466161f7ba56442227f2953e54
sam <samuel1haim@gmail.com>
2021-04-12 23:09:50 +0100

    plumbing the user router into index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const mongoose =  require('mongoose');
+const Joi = require('joi');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
+mongoose.connect('mongodb://localhost/mlc')
+	.then(() => console.log('connect to mongodb...'))
+	.catch(err => console.error('coould not connect to mongodb', err));
+
+app.use(express.json());
+app.use('/api/users', users);
 
 // PORT
 const port = process.env.PORT || 3000;
