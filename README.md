c62755391a764f3c19a5164c4a7da5d5e86ab1ee
sam <samuel1haim@gmail.com>
2021-04-12 20:23:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Setting the environment ###
+
+1. Install express
+```$ npm init --yes```
+```npm i express```
+
+2. Create file index.js
+
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
